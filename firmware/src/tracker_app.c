0ab20fb1e84abc4d428e170fc9910257e9186c18
James Hagerman <james.hagerman@gmail.com>
2018-06-13 01:52:08 -0700

    Adding some text to make it obvious I am actually working on this

--- firmware/src/tracker_app.c
+++ firmware/src/tracker_app.c
@@ -37,8 +37,14 @@
     #define ASD  ASD_12BIT
 #endif
 
-//static const gfx_rgb565 bg_color  = 0xF81F;
-//static const gfx_rgb565 vol_color = 0x07E0;
+// Python conversion:
+// def rgb_hex565(red, green, blue):
+//    # take in the red, green and blue values (0-255) as 8 bit values and then combine
+//    # and shift them to make them a 16 bit hex value in 565 format.
+//    print ("0x%0.4X" % ((int(red / 255 * 31) << 11) | (int(green / 255 * 63) << 5) | (int(blue / 255 * 31))))
+
+static const gfx_rgb565 bg_color  = 0x0000; // black!
+static const gfx_rgb565 vol_color = 0xFFE0; // yellow
 
 //static uint8_t prev_vol;
 
@@ -75,15 +81,15 @@ void tracker_animate(void) {
 
 void tracker_render(void) {
   // For now, just render somthing simple...
-  //if (lcd_bg_color() != bg_color) {
-  //  lcd_set_bg_color(bg_color, true);
-  //}
+  if (lcd_bg_color() != bg_color) {
+    lcd_set_bg_color(bg_color, true);
+  }
 
   for (size_t y = 0, h; y < LCD_HEIGHT; y += h) {
     h = MIN(LCD_MAX_SLICE_ROWS, LCD_HEIGHT - y);
     gfx_pixslice *slice = lcd_alloc_pixslice(0, y, LCD_WIDTH, h);
     
-    //text_draw_str16(slice, "Volume: ", 4, 10, vol_color); 
+    text_draw_str16(slice, "Tracker! ", 0, 0, vol_color); 
 
     //audio_render_slice(slice);
     lcd_send_pixslice(slice);
